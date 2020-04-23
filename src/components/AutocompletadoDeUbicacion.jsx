cee4b98258a74eb9e6f1383fe1bbfd1250e12430
cassa10 <cassanojoseluis@gmail.com>
2020-04-22 23:59:38 -0300

    Borro import que no se usa

--- src/components/AutocompletadoDeUbicacion.jsx
+++ src/components/AutocompletadoDeUbicacion.jsx
@@ -1,6 +1,5 @@
 import {makeStyles} from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
 import AddLocationIcon from '@material-ui/icons/AddLocation';
 import ErrorIcon from "@material-ui/icons/Error";
 import DoneIcon from "@material-ui/icons/Done";

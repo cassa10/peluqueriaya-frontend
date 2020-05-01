5b531c202b7fdb52c4f2eb63d1a3510c1cf8a97c
cassa10 <cassanojoseluis@gmail.com>
2020-05-01 18:02:10 -0300

    Agrego input para buscar por nombre o tipo y un boton para reiniciar el filtrado.

--- src/views/PaginaBusquedaPeluqueros.jsx
+++ src/views/PaginaBusquedaPeluqueros.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import Box from "@material-ui/core/Box";
+import {Button, Box, TextField} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 import ListaPeluqueros from '../components/ListaPeluqueros';
-import Button from "@material-ui/core/Button";
 import useServicioDePeluquero from "../service/useServicioDePeluquero";
 import {useHistory} from "react-router";
 import TabDeFiltradoPorServicio from "../components/TabDeFiltradoPorServicio";
 
+const useStyles = makeStyles((theme) => ({
+    textfield1: {
+        width: '100%'
+    },
+    buttonSearch: {
+        width: '50%'
+    },
+    buttonReset: {
+        width: '50%'
+    }
+  }));
 
 const PaginaBusquedaPeluqueros = () => {
+    const classes = useStyles();
     const [resultados, setResultados] = useState([]);
-    const [{buscarPeluquero},{buscarPeluquerosPorTipoDeServicio}] = useServicioDePeluquero();
+    const [{buscarPeluquero},{buscarPeluquerosPorTipoDeServicio}, {buscarPeluquerosPorNombreOTipo}] = useServicioDePeluquero();
+    const [nombreOTipo,setNombreOTipo] = useState('');
     const {push} = useHistory();
 
     useEffect(() => {
@@ -23,10 +36,28 @@ const PaginaBusquedaPeluqueros = () => {
         buscarPeluquerosPorTipoDeServicio(tipoDeServicio, (resultados) => setResultados(resultados));
     }
 
+    const buscarPorNombreOTipo = (nombreOTipo) => {
+        buscarPeluquerosPorNombreOTipo(nombreOTipo, (resultados) => setResultados(resultados)); 
+    }
+
+    const handleNombreTipoInput = (event) => {
+        setNombreOTipo(event.target.value)
+    }
+    
+    const reiniciarFiltros = () => {
+        setNombreOTipo('')
+        buscarPorNombreOTipo(' ')
+    }
+
     return (
         <div>
             <Box bgcolor="primary.main" color="primary.contrastText" textAlign="center" m={2}>
                 <TabDeFiltradoPorServicio buscar={buscarPorTipoDeServicio}/>
+            </Box>
+            <Box bgcolor="primary.main" color="primary.contrastText" textAlign="center" m={2}>
+                <TextField className={classes.textfield1} color="secondary" label="Filtrar por nombre o tipo" value={nombreOTipo} variant="filled" onChange={handleNombreTipoInput}/>
+                <Button className={classes.buttonReset} onClick={reiniciarFiltros}>Reiniciar</Button>
+                <Button className={classes.buttonSearch} onClick={() => buscarPorNombreOTipo(nombreOTipo)}>Buscar</Button>
             </Box>
             <ListaPeluqueros
                 resultados={resultados}

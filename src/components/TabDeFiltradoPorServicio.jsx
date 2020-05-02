faf55db7e127fa00a0272419b8f951449a96fea2
LisaR <lisar.3467@gmail.com>
2020-05-01 22:35:39 -0300

    Dev (#14)
    * Buscar por ubicacion cliente (#1)
    
    * se crea la vista de home page sin logica
    
    * se crea carpeta views
    
    * Buscar por ubicacion cliente (#2)
    
    * se crea la vista de home page sin logica
    
    * se crea carpeta views
    
    * funciona autocomplete con ubicacion del navegador, se empieza a hacer autocomplete con input del usuario, se agregan paquetes material lab y es-lint
    
    * fix a autocomplete de direccion
    
    * se agregan validaciones y un icono para indicar si la direccion esta verificada
    
    * Buscar por ubicacion cliente fixes (#3)
    
    * componente LocationAutocomplete mas flexible y borrado archivos innecesarios
    
    * View search peluqueros cercanos (#5)
    
    * Pasando props por los componentes. Creando componentes basicos y ruta /search para visualizar resultados de la busqueda
    
    * Visualizacion de peluqueros cercanos.
    
    * Solve compilation bug, change dist name folder to css
    
    * Arreglos de checkpoint (#6)
    
    * traducir funciones y variables al español
    
    * MapasAPI.jsx usa api.js para hacer las request y se le delega mas funciones; se saca proxy de package.json
    
    * Checkpoint fixes (#7)
    
    * Hago responsive la imagen del logo y borro css
    
    * Borrando props y sustituyendolas
    
    * Mas responsive search bar
    
    * Borro import que no se usa
    
    * Manejo de api (#9)
    
    * agregado un contexto para manejar request a api globalmente y creado un servicio para manejar las request especificas en AutocompletadoDeUbicacion
    
    * se empieza a hacer el manejo de errores
    
    * se agrega logica de manejo de errores
    
    * se agrega spinner de cargando en AutocompletadoDeUbicacion.jsx
    
    * se cambian todos los llamados a api con el metodo service y se agrega error 404 en route
    
    * Filtrar por servicio (#10)
    
    * se agrega tab de filtros por tipo de servicio que contiene el peluquero
    
    * se guardan las fotos de los tipos de servicios y font en assets y se corrige
    
    * Filtrar por nombre y tipo (#11)
    
    * Saco el boton de navbar y agrego logo icon responsive que redirigie al home.
    
    * Agrego input para buscar por nombre o tipo y un boton para reiniciar el filtrado.
    
    * El boton de reiniciar filtros deselecciona el tab
    
    * Cambiando valor '' a false por un warning de material
    
    * Visualizar servicios peluquero (#12)
    
    * se arregla bug de componente actualizado sin montar (#13)
    
    Co-authored-by: Jose Cassano <39202199+cassa10@users.noreply.github.com>

---
+++ src/components/TabDeFiltradoPorServicio.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import useServicioDeServicio from "../service/useServicioDeServicio";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+    img: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+    },
+}));
+
+const TabDeFiltradoPorServicio = ({buscar, tipoDeServicio, setTipoDeServicio}) => {
+    const classes = useStyles();
+    const [tiposDeServicio, setTiposDeServicio] = useState([]);
+    const [{obtenerTiposDeServicio, cargandoTDS}] = useServicioDeServicio();
+
+    useEffect(() => {
+        let cancel = false;
+        const f = () => {
+            if (cancel) {
+                obtenerTiposDeServicio((tiposDeServicio) => setTiposDeServicio(tiposDeServicio));
+            }
+        }
+        f();
+        return () => {
+            cancel = true
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    const handleChange = (event, nuevoTipoDeServicio) => {
+        setTipoDeServicio(nuevoTipoDeServicio);
+        buscar(nuevoTipoDeServicio);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                {!cargandoTDS &&
+                <Tabs
+                    value={tipoDeServicio}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons="on"
+                    indicatorColor="primary"
+                    textColor="primary"
+                >
+                    {tiposDeServicio.map((tipo) =>
+                        <Tab label={tipo.nombre}
+                             icon={<img className={classes.img}
+                                        src={require(`../assets/images/${tipo.id}.jpg`)}
+                                        alt="logo"/>}
+                             key={tipo.id}
+                             value={tipo.id}/>)}
+                </Tabs>}
+            </AppBar>
+        </div>
+    );
+};
+
+export default TabDeFiltradoPorServicio;
--- src/views/PaginaBusquedaPeluqueros.jsx
+++ src/views/PaginaBusquedaPeluqueros.jsx
@@ -1,58 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid"
+import {Button, Box, TextField} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 import ListaPeluqueros from '../components/ListaPeluqueros';
-import Button from "@material-ui/core/Button";
-import API from '../service/api';
-import {makeStyles} from "@material-ui/core/styles";
+import useServicioDePeluquero from "../service/useServicioDePeluquero";
+import {useHistory} from "react-router";
+import TabDeFiltradoPorServicio from "../components/TabDeFiltradoPorServicio";
 
-const useStyles = makeStyles(() => ({
-    img: {
-        margin: 'auto',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
-        cursor: 'pointer',
+const useStyles = makeStyles((theme) => ({
+    textfield1: {
+        width: '100%'
     },
-}));
+    buttonSearch: {
+        width: '50%'
+    },
+    buttonReset: {
+        width: '50%'
+    }
+  }));
 
 const PaginaBusquedaPeluqueros = () => {
-    const clases = useStyles();
-    const [ubicacion] = useState(
-        {
-            latitude: sessionStorage.getItem('userLocationLatitude'),
-            longitude: sessionStorage.getItem('userLocationLongitude')
-        });
-
-    const [resultados, setResultados] = useState([])
-
-    const irPaginaPrincipal = () => {
-        window.location.href = '/';
-    };
+    const classes = useStyles();
+    const [resultados, setResultados] = useState([]);
+    const [{buscarPeluquero},{buscarPeluquerosPorTipoDeServicio}, {buscarPeluquerosPorNombreOTipo}] = useServicioDePeluquero();
+    const [nombreOTipo,setNombreOTipo] = useState('');
+    const [tipoDeServicio, setTipoDeServicio] = useState(false);
+    const {push} = useHistory();
 
     useEffect(() => {
-        if (!ubicacion.latitude || !ubicacion.longitude) {
-            irPaginaPrincipal()
-        }
-        API.get('/peluquero/search', ubicacion)
-            .then((response) => setResultados(response))
-            .catch((error) => console.log(error))
-    },[ubicacion]);
+        buscarPeluquero((peluqueros) => setResultados(peluqueros));
+        // eslint-disable-next-line
+    },[]);
 
+    const irPaginaPrincipal = () => push("/");
+
+    const buscarPorTipoDeServicio = (tipoDeServicio) => {
+        buscarPeluquerosPorTipoDeServicio(tipoDeServicio, (resultados) => setResultados(resultados));
+    }
+
+    const buscarPorNombreOTipo = (nombreOTipo) => {
+        buscarPeluquerosPorNombreOTipo(nombreOTipo, (resultados) => setResultados(resultados)); 
+    }
+
+    const handleNombreTipoInput = (event) => {
+        setNombreOTipo(event.target.value)
+    }
+    
+    const reiniciarFiltros = () => {
+        setNombreOTipo('')
+        setTipoDeServicio(false)
+        buscarPorNombreOTipo(' ')
+    }
 
     return (
         <div>
             <Box bgcolor="primary.main" color="primary.contrastText" textAlign="center" m={2}>
-                <Grid container justify="center">
-                    <img
-                        className={clases.img}
-                        src={'peluqueriaya-logo.png'}
-                        alt="logo"
-                        onClick={irPaginaPrincipal}
-                    />
-                </Grid>
+                <TabDeFiltradoPorServicio buscar={buscarPorTipoDeServicio} tipoDeServicio={tipoDeServicio} setTipoDeServicio={setTipoDeServicio}/>
             </Box>
-            
+            <Box bgcolor="primary.main" color="primary.contrastText" textAlign="center" m={2}>
+                <TextField className={classes.textfield1} color="secondary" label="Filtrar por nombre o tipo" value={nombreOTipo} variant="filled" onChange={handleNombreTipoInput}/>
+                <Button className={classes.buttonReset} onClick={reiniciarFiltros}>Reiniciar</Button>
+                <Button className={classes.buttonSearch} onClick={() => buscarPorNombreOTipo(nombreOTipo)}>Buscar</Button>
+            </Box>
             <ListaPeluqueros
                 resultados={resultados}
                 botonIrPaginaPrincipal={

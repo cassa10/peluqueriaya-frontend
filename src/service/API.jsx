04d39156a83637e018461844e6b59c6b5669b550
lisar01 <lisar.3467@gmail.com>
2020-05-30 21:16:36 -0300

    se crean metodos de llamados a api con token

--- src/service/API.jsx
+++ src/service/API.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {useManejadorDeErrores} from "../contexts/errors/ManejadorDeErrores";
 import axios from 'axios';
+import {useUser} from "../contexts/UserProvider";
 
 // eslint-disable-next-line no-undef
 const server = process.env.REACT_APP_APIBACKEND || 'http://localhost:8080';
@@ -9,6 +10,7 @@ const usarAPI = (metodo) => (path, fDatos = () => {}, parametrosIniciales = null
     const [parametros, setParametros] = useState(parametrosIniciales);
     const [cargando, setCargando] = useState(false);
     const {setError} = useManejadorDeErrores();
+    const {getTokenSilently} = useUser();
 
     useEffect(() => {
         let cancelar = false;
@@ -16,7 +18,7 @@ const usarAPI = (metodo) => (path, fDatos = () => {}, parametrosIniciales = null
             if (parametros !== null) {
                 setCargando(true);
                 try {
-                    const {data} = await metodo(`${server}${path}`, parametros);
+                    const {data} = await metodo(`${server}${path}`, {parametros, getTokenSilently});
                     if (!cancelar) {
                         setCargando(false);
                         fDatos(data);
@@ -40,7 +42,7 @@ const usarAPI = (metodo) => (path, fDatos = () => {}, parametrosIniciales = null
     return {cargando, parametros, setParametros}
 }
 
-export const useGet = usarAPI((path, parametros) => {
+export const useGet = usarAPI((path, {parametros}) => {
     const params = new URLSearchParams();
     Object.entries(parametros).forEach(([name, value]) => {
         if (Array.isArray(value)) value.forEach((valueDeValue) => params.append(name, valueDeValue));
@@ -48,7 +50,22 @@ export const useGet = usarAPI((path, parametros) => {
     });
     return axios.get(path, {params: params});
 });
+export const usePost = usarAPI((path, {parametros}) => axios.post(path, parametros));
+export const usePut = usarAPI((path, {parametros}) => axios.put(path, parametros));
+export const useDelete = usarAPI((path, {parametros}) => axios.delete(path, {params: parametros}));
 
-export const usePost = usarAPI(axios.post);
-export const usePut = usarAPI(axios.put);
-export const useDelete = usarAPI(axios.delete);
+
+const headers = (token) => ({ headers: {Authorization: `Bearer ${token}`}});
+
+export const useGetConAuth = usarAPI(async(path, {parametros, getTokenSilently}) => {
+    return axios.get(path, {params: parametros , ...headers(await getTokenSilently())})
+});
+export const usePostConAuth = usarAPI(async(path, {parametros, getTokenSilently}) => {
+    return axios.post(path, parametros , headers(await getTokenSilently()))
+});
+export const usePutConAuth = usarAPI(async(path, {parametros, getTokenSilently}) => {
+    return axios.put(path, parametros , headers(await getTokenSilently()))
+});
+export const useDeleteConAuth = usarAPI(async(path, {parametros, getTokenSilently}) => {
+    return axios.delete(path, {params: parametros , ...headers(await getTokenSilently())})
+});
